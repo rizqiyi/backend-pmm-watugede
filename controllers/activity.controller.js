5304bc49612eb7cec90f899732844c5dfe479886
Rizqiyanto Imanullah <55864145+rizqiyi@users.noreply.github.com>
2021-01-04 06:01:33 +0700

    add endpoint when admin was logged out

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -1,4 +1,5 @@
 const ActivitySchema = require("../models/activity.model");
+const AdminSchema = require("../models/admin.model");
 
 exports.getActivityData = async (req, res) => {
   try {
@@ -8,6 +9,42 @@ exports.getActivityData = async (req, res) => {
       success: true,
       message: "Succesfully fetch your data",
       data: all,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+exports.postActivityWhenLogOut = async (req, res) => {
+  try {
+    const findUser = await AdminSchema.findById(req.params.id);
+
+    if (!findUser)
+      return res.status(400).json({
+        success: false,
+        message: "Admin Not Found",
+      });
+
+    const activity = await ActivitySchema.create({
+      activity_name: `${findUser.nama_lengkap} Telah Logout.`,
+      activity_by: findUser._id,
+    });
+
+    await AdminSchema.findByIdAndUpdate(
+      { _id: findUser._id },
+      {
+        $push: {
+          activity: activity._id,
+        },
+      }
+    );
+
+    return res.status(201).json({
+      success: true,
+      message: "Sukses Logout",
     });
   } catch (err) {
     return res.status(500).json({
--- routes/activity.route.js
+++ routes/activity.route.js
@@ -1,10 +1,23 @@
 const express = require("express");
 
-const { getActivityData } = require("../controllers/activity.controller");
+const {
+  getActivityData,
+  postActivityWhenLogOut,
+} = require("../controllers/activity.controller");
 const middleware = require("../middlewares/auth");
 
 const router = express.Router();
 
+//@desc     GET Data Activity
+//@routes   GET
+//@endpoint /api/activity
+//@access   Private
 router.route("/").all(middleware).get(getActivityData);
+
+//@desc     POST Data Activity
+//@routes   POST
+//@endpoint /api/activity/:id
+//@access   Private
+router.route("/:id").all(middleware).post(postActivityWhenLogOut);
 
 module.exports = router;
