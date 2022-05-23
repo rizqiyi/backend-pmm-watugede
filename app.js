6dfc75d1555d2db604c759b66f1f5b60866b0fe7
Rizqiyanto Imanullah <human.gesture@gmail.com>
2022-05-24 00:34:56 +0700

    fix: disable cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "assets")));
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/", (req, res) => res.send("API Desa Watugede"));
 
